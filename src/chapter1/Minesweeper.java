62df754862f3a11384b35624922208994c8195c0
Zhenlei Ji <zhenleij@motorola.com>
2016-04-23 00:52:49 -0300

    Fixed bug related to null attribution, just change to characther '0'

--- src/chapter1/Minesweeper.java
+++ src/chapter1/Minesweeper.java
@@ -34,7 +34,7 @@ public class Minesweeper {
                 for (int j = 0; j < m; j++) {
                     if (matrix[i + 1][j + 1] != '*') {
                         int p = 0; // p = 0 to loop correctly
-                        matrix[i + 1][j + 1] = 0;
+                        matrix[i + 1][j + 1] = '0';
                         for (int line = -1; line < 2; line++) {
                             for (int column = -1; column < 2; column++) {
                                 if (matrix[i + 1 + line][j + 1 + column] == '*') {
@@ -46,18 +46,19 @@ public class Minesweeper {
                     }
                 }
             }
-            System.out.print(String.format("Field #%d:", count++));
-            printMatrix(matrix);
+            System.out.println(String.format("Field #%d:", count++));
+            printMatrix(matrix, n, m);
+            System.out.println();
 
             n = scanner.nextInt();
             m = scanner.nextInt();
         }
     }
 
-    private static void printMatrix(char[][] matrix) {
-        for (char[] line : matrix) {
-            for (char c : line) {
-                System.out.printf(String.valueOf(c));
+    private static void printMatrix(char[][] matrix, int n, int m) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.printf(String.valueOf(matrix[i][j]));
             }
             System.out.println();
         }

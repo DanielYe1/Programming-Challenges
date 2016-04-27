af4d650e9cdf43d563030a23334cceeefb9163e5
Daniel Ye <danielxinxin1@gmail.com>
2016-04-26 22:55:45 -0300

    Implemented Check the Check(without loop), page 23.

--- src/chapter1/P23ChecktheCheck.java
+++ src/chapter1/P23ChecktheCheck.java
@@ -2,10 +2,19 @@ package chapter1;
 
 import java.util.Scanner;
 
+import static java.awt.Color.white;
+
 /**
  * Created by Daniel on 24/04/2016.
  */
 public class P23ChecktheCheck {
+
+    public static class bool {
+        public static boolean white = false;
+        public static boolean black = false;
+        public static boolean none = false;
+    }
+
     public enum movements {
         NORTHEAST,
         NORTHWEST,
@@ -20,7 +29,6 @@ public class P23ChecktheCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] input = new String[8]; // start putting in the matrix
-
         for (int i = 0; i < 8; i++) {
             input[i] = scanner.next();
         }
@@ -38,104 +46,102 @@ public class P23ChecktheCheck {
                 matrix[i + 2][j + 2] = input[i].charAt(j);
             }
         }
-        printMatrix(matrix, 8, 8); // debug matrix
 
-        // for the part of cols and rows, can put enums, causa the queen will use the same object as
-        // the rook and the bishop
-        // bishop seems the most difficult
-        // rook can use some cols and rows logic
-        // pawn,king aren't hardly to do
-        // no logic to do with knight
 
         for (int i = 2; i < 10; i++) {
             for (int j = 2; j < 10; j++) {
                 switch (matrix[i][j]) {
                     case 'p':
                         if (matrix[i + 1][j - 1] == 'K' || matrix[i + 1][j + 1] == 'K') {
-                            System.out.println("Pawn Check!");
+                            bool.white = true;
                         }
                         break;
                     case 'P':
                         if (matrix[i - 1][j - 1] == 'k' || matrix[i - 1][j + 1] == 'k') {
-                            System.out.println("Pawn Check!");
+                            bool.black = true;
                         }
                         break;
                     case 'n':
                         if (matrix[i + 2][j + 1] == 'K' || matrix[i + 2][j - 1] == 'K' || matrix[i - 2][j - 1] == 'K' || matrix[i - 2][j + 1] == 'K' || matrix[i + 1][j + 2] == 'K' || matrix[i - 1][j + 2] == 'K' || matrix[i + 1][j - 2] == 'K' || matrix[i - 1][j - 2] == 'K') {
-                            System.out.println("Knight Check!");
+                            bool.white = true;
                         }
                         break;
                     case 'N':
                         if (matrix[i + 2][j + 1] == 'k' || matrix[i + 2][j - 1] == 'k' || matrix[i - 2][j - 1] == 'k' || matrix[i - 2][j + 1] == 'k' || matrix[i + 1][j + 2] == 'k' || matrix[i - 1][j + 2] == 'k' || matrix[i + 1][j - 2] == 'k' || matrix[i - 1][j - 2] == 'k') {
-                            System.out.println("Knight Check!");
+                            bool.black = true;
                         }
                         break;
                     case 'b':  // work with bugs, bug fix tomorrow;
-                        setPart(matrix, movements.NORTHEAST,i,j);
-                        setPart(matrix, movements.NORTHWEST,i,j);
-                        setPart(matrix, movements.SOUTHEAST,i,j);
-                        setPart(matrix, movements.SOUTHWEST,i,j);
+                        setPart(matrix, movements.NORTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHWEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHWEST, i, j, matrix[i][j]);
                     case 'B':  // work with bugs, bug fix tomorrow;
-                        setPart(matrix, movements.NORTHEAST,i,j);
-                        setPart(matrix, movements.NORTHWEST,i,j);
-                        setPart(matrix, movements.SOUTHEAST,i,j);
-                        setPart(matrix, movements.SOUTHWEST,i,j);
+                        setPart(matrix, movements.NORTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHWEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHWEST, i, j, matrix[i][j]);
                     case 'r':
-                        setPart(matrix, movements.EAST,i,j);
-                        setPart(matrix, movements.NORTH,i,j);
-                        setPart(matrix, movements.SOUTH,i,j);
-                        setPart(matrix, movements.WEST,i,j);
+                        setPart(matrix, movements.EAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.WEST, i, j, matrix[i][j]);
                         break;
                     case 'R':
-                        setPart(matrix, movements.EAST,i,j);
-                        setPart(matrix, movements.NORTH,i,j);
-                        setPart(matrix, movements.SOUTH,i,j);
-                        setPart(matrix, movements.WEST,i,j);
+                        setPart(matrix, movements.EAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.WEST, i, j, matrix[i][j]);
                         break;
                     case 'q':
-                        setPart(matrix, movements.EAST,i,j);
-                        setPart(matrix, movements.NORTH,i,j);
-                        setPart(matrix, movements.SOUTH,i,j);
-                        setPart(matrix, movements.WEST,i,j);
-                        setPart(matrix, movements.NORTHEAST,i,j);
-                        setPart(matrix, movements.NORTHWEST,i,j);
-                        setPart(matrix, movements.SOUTHEAST,i,j);
-                        setPart(matrix, movements.SOUTHWEST,i,j);
+                        setPart(matrix, movements.EAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.WEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHWEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHWEST, i, j, matrix[i][j]);
                     case 'Q':
-                        setPart(matrix, movements.EAST,i,j);
-                        setPart(matrix, movements.NORTH,i,j);
-                        setPart(matrix, movements.SOUTH,i,j);
-                        setPart(matrix, movements.WEST,i,j);
-                        setPart(matrix, movements.NORTHEAST,i,j);
-                        setPart(matrix, movements.NORTHWEST,i,j);
-                        setPart(matrix, movements.SOUTHEAST,i,j);
-                        setPart(matrix, movements.SOUTHWEST,i,j);
+                        setPart(matrix, movements.EAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTH, i, j, matrix[i][j]);
+                        setPart(matrix, movements.WEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.NORTHWEST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHEAST, i, j, matrix[i][j]);
+                        setPart(matrix, movements.SOUTHWEST, i, j, matrix[i][j]);
                 }
             }
         }
 
-    }
-
-    private static void printMatrix(char[][] matrix, int n, int m) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf(String.valueOf(matrix[i + 2][j + 2]));
-            }
-            System.out.println();
+        if (bool.white) {
+            System.out.println("white king is in check.");
+        } else if (bool.black) {
+            System.out.println("black king is in check.");
+        } else {
+            System.out.println("no king is in check.");
         }
     }
 
-    private static void setPart(char[][] matrix, movements part,int i, int j) {
+
+    public static void setPart(char[][] matrix, movements part, int i, int j, char piece) {
         switch (part) {
             case NORTHEAST:
                 for (int k = 1; k < 8; k++) {
                     for (int l = 1; l < 8; l++) {
-                        if ((i - k) <= 2 || (j + k) >= 10) {
+                        if ((i - k) < 2 || (j + k) >= 10) {
                             break;
                         }
                         if (matrix[i - k][j + k] != '.') {
-                            if (matrix[i - k][j + k] == 'k'|| matrix[i - k][j + k] == 'K') {
-                                System.out.println("bishop check");
+                            if (matrix[i - k][j + k] == 'k' || matrix[i - k][j + k] == 'K') {
+                                if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                    bool.white = true;
+
+                                } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                    bool.black = true;
+
+                                }
                                 break;
                             } else {
                                 break;
@@ -151,8 +157,14 @@ public class P23ChecktheCheck {
                             break;
                         }                                   // index out
                         if (matrix[i - k][j - k] != '.') {
-                            if (matrix[i - k][j - k] == 'k'|| matrix[i - k][j - k] == 'K') {
-                                System.out.println("bishop check");
+                            if (matrix[i - k][j - k] == 'k' || matrix[i - k][j - k] == 'K') {
+                                if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                    bool.white = true;
+
+                                } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                    bool.black = true;
+
+                                }
                                 break;
                             } else {
                                 break;
@@ -168,8 +180,14 @@ public class P23ChecktheCheck {
                             break;
                         }
                         if (matrix[i + k][j + k] != '.') {
-                            if (matrix[i + k][j + k] == 'k'|| matrix[i + k][j + k] == 'K') {
-                                System.out.println("bishop check");
+                            if (matrix[i + k][j + k] == 'k' || matrix[i + k][j + k] == 'K') {
+                                if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                    bool.white = true;
+
+                                } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                    bool.black = true;
+
+                                }
                                 break;
                             } else {
                                 break;
@@ -181,12 +199,18 @@ public class P23ChecktheCheck {
             case SOUTHWEST:
                 for (int k = 1; k < 8; k++) {
                     for (int l = 1; l < 8; l++) {
-                        if ((i + k) >= 10 || (j - k) <= 2) {
+                        if ((i + k) >= 10 || (j - k) < 2) {
                             break;
                         }
                         if (matrix[i + k][j - k] != '.') {
-                            if (matrix[i + k][j - k] == 'k'|| matrix[i + k][j - k] == 'K') {
-                                System.out.println("bishop check");
+                            if (matrix[i + k][j - k] == 'k' || matrix[i + k][j - k] == 'K') {
+                                if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                    bool.white = true;
+
+                                } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                    bool.black = true;
+
+                                }
                                 break;
                             } else {
                                 break;
@@ -197,12 +221,17 @@ public class P23ChecktheCheck {
                 break;
             case NORTH:
                 for (int k = 1; k < 8; k++) {
-                    if ((i - k) <= 2) {
+                    if ((i - k) < 2) {
                         break;
                     }
                     if (matrix[i - k][j] != '.') {
-                        if (matrix[i - k][j] == 'k'|| matrix[i - k][j] == 'K') {
-                            System.out.println("rook check");
+                        if (matrix[i - k][j] == 'k' || matrix[i - k][j] == 'K') {
+                            if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                bool.white = true;
+                            } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                bool.black = true;
+
+                            }
                             break;
                         } else {
                             break;
@@ -216,8 +245,13 @@ public class P23ChecktheCheck {
                         break;
                     }
                     if (matrix[i + k][j] != '.') {
-                        if (matrix[i + k][j] == 'k'|| matrix[i + k][j] == 'K') {
-                            System.out.println("rook check");
+                        if (matrix[i + k][j] == 'k' || matrix[i + k][j] == 'K') {
+                            if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                bool.white = true;
+                            } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                bool.black = true;
+
+                            }
                             break;
                         } else {
                             break;
@@ -227,12 +261,17 @@ public class P23ChecktheCheck {
                 break;
             case WEST:
                 for (int k = 1; k < 8; k++) {
-                    if ((j - k) <= 2) {
+                    if ((j - k) < 2) {
                         break;
                     }
                     if (matrix[i][j - k] != '.') {
-                        if (matrix[i][j - k] == 'k'|| matrix[i][j - k] == 'K') {
-                            System.out.println("rook check");
+                        if (matrix[i][j - k] == 'k' || matrix[i][j - k] == 'K') {
+                            if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                bool.white = true;
+                            } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                bool.black = true;
+
+                            }
                             break;
                         } else {
                             break;
@@ -242,12 +281,17 @@ public class P23ChecktheCheck {
                 break;
             case EAST:
                 for (int k = 1; k < 8; k++) {
-                    if ((j + k) <= 10) {
+                    if ((j + k) >= 11) {
                         break;
                     }
                     if (matrix[i][j + k] != '.') {
-                        if (matrix[i][j + k] == 'k'|| matrix[i][j + k] == 'K') {
-                            System.out.println("rook check");
+                        if (matrix[i][j + k] == 'k' || matrix[i][j + k] == 'K') {
+                            if (piece == 'b' || piece == 'q' || piece == 'r') {
+                                bool.white = true;
+                            } else if (piece == 'B' || piece == 'Q' || piece == 'R') {
+                                bool.black = true;
+
+                            }
                             break;
                         } else {
                             break;
---
+++ src/chapter1/test1.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+/**
+ * Created by Daniel on 26/04/2016.
+ */
+public class test1 {
+}
